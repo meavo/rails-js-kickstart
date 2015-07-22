8a53847a7b2c0169e5f69c0310d7b716a90f2781
Michaël van Oosten <michael.vanoosten@mangrove.nl>
2015-07-22 15:16:19 +0200

    Small tweak.

--- lib/rails/js/kickstart.rb
+++ lib/rails/js/kickstart.rb
@@ -3,7 +3,8 @@ require "rails/js/kickstart/version"
 module Rails
   module Js
     module Kickstart
-      class Engine < ::Rails::Engine; end
+      class Engine < ::Rails::Engine
+      end
     end
   end
 end
